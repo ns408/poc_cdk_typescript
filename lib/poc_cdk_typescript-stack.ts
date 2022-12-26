459ca78a97b509525a5d70ac1f6197947c0dc478
ns408 <nsrea408@gmail.com>
2022-12-27 09:43:26 +1100

    Clean up

--- lib/poc_cdk_typescript-stack.ts
+++ lib/poc_cdk_typescript-stack.ts
@@ -1,28 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-//import * as ec2 from "@aws-cdk/aws-ec2";            // Allows working with EC2 and VPC resources
-//import * as iam from "@aws-cdk/aws-iam";            // Allows working with IAM resources
-//import * as s3assets from "@aws-cdk/aws-s3-assets"; // Allows managing files with S3
-import * as keypair from "cdk-ec2-key-pair";        // Helper to create EC2 SSH keypairs
-import * as path from "path";                       // Helper for working with file paths
+import * as ec2 from "aws-cdk-lib/aws-ec2";             // Allows working with EC2 and VPC resources
+import * as iam from "aws-cdk-lib/aws-iam";             // Allows working with IAM resources
+import * as s3_assets from "aws-cdk-lib/aws-s3-assets"; // Allows managing files with S3
+import * as keypair from "cdk-ec2-key-pair";            // Helper to create EC2 SSH keypairs
+import * as path from "path";                           // Helper for working with file paths
 
-import variablesFile from '../variables_file.json'; // Import from a file
+import variablesFile from '../variables_file.json';     // Import some variables from a json file
 
-export class PocCdkTypescriptStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class PocCdkTypescriptStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
     // console.log(variablesFile.vpcid)
-    const vpc = cdk.aws_ec2.Vpc.fromLookup(this, 'VPC', { vpcId: variablesFile.vpcid }
-      // {tags: {'aws-cdk:Name': variablesFile.vpcname}}
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
+      vpcId: variablesFile.vpcid }
     );
-
-    //ec2.Vpc.fromVpcAttributes(this, 'MyVPC', {
-    //  vpcId: variablesFile.vpcid,
-    //});
 
     // Create a key pair to be used with this EC2 Instance
     const key = new keypair.KeyPair(this, "KeyPair", {
@@ -32,7 +28,7 @@ export class PocCdkTypescriptStack extends cdk.Stack {
     key.grantReadOnPublicKey;
 
     // Security group for the EC2 instance
-    const securityGroup = new cdk.aws_ec2.SecurityGroup(this, "SecurityGroup", {
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
       vpc,
       description: "Allow SSH (TCP port 22) and HTTP (TCP port 80) in",
       allowAllOutbound: true,
@@ -40,40 +36,40 @@ export class PocCdkTypescriptStack extends cdk.Stack {
 
     // Allow SSH access on port tcp/22
     securityGroup.addIngressRule(
-      cdk.aws_ec2.Peer.ipv4(variablesFile.whitelistIP),
-      cdk.aws_ec2.Port.tcp(22),
+      ec2.Peer.ipv4(variablesFile.whitelistIP),
+      ec2.Port.tcp(22),
       "Allow SSH Access"
     );
 
     // Allow HTTP access on port tcp/80
     securityGroup.addIngressRule(
-      cdk.aws_ec2.Peer.ipv4(variablesFile.whitelistIP),
-      cdk.aws_ec2.Port.tcp(80),
+      ec2.Peer.ipv4(variablesFile.whitelistIP),
+      ec2.Port.tcp(80),
       "Allow HTTP Access"
     );
 
     // IAM role to allow access to other AWS services
-    const role = new cdk.aws_iam.Role(this, "ec2Role", {
-      assumedBy: new cdk.aws_iam.ServicePrincipal("ec2.amazonaws.com"),
+    const role = new iam.Role(this, "ec2Role", {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
     });
 
     // IAM policy attachment to allow access to
     role.addManagedPolicy(
-      cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
     );
 
     // Look up the AMI Id for the Amazon Linux 2 Image with CPU Type X86_64
-    const ami = new cdk.aws_ec2.AmazonLinuxImage({
-      generation: cdk.aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      cpuType: cdk.aws_ec2.AmazonLinuxCpuType.X86_64,
+    const ami = new ec2.AmazonLinuxImage({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.X86_64,
     });
 
     // Create the EC2 instance using the Security Group, AMI, and KeyPair defined.
-    const ec2Instance = new cdk.aws_ec2.Instance(this, "MyInstance", {
+    const ec2Instance = new ec2.Instance(this, "MyInstance", {
       vpc,
-      instanceType: cdk.aws_ec2.InstanceType.of(
-        cdk.aws_ec2.InstanceClass.T2,
-        cdk.aws_ec2.InstanceSize.MICRO
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
       ),
       machineImage: ami,
       securityGroup: securityGroup,
@@ -85,7 +81,7 @@ export class PocCdkTypescriptStack extends cdk.Stack {
 
     // --- Sample App ---
     // Upload the sample app  to S3
-    const sampleAppAsset = new cdk.aws_s3_assets.Asset(this, "SampleAppAsset", {
+    const sampleAppAsset = new s3_assets.Asset(this, "SampleAppAsset", {
       path: path.join(__dirname, "../../SampleApp"),
     });
 
@@ -102,7 +98,7 @@ export class PocCdkTypescriptStack extends cdk.Stack {
 
     // --- Configuration Script ---
     // Upload the configuration file to S3
-    const configScriptAsset = new cdk.aws_s3_assets.Asset(this, "ConfigScriptAsset", {
+    const configScriptAsset = new s3_assets.Asset(this, "ConfigScriptAsset", {
       path: path.join(__dirname, "../../SampleApp/configure_amz_linux_sample_app.sh"),
     });
 
@@ -124,18 +120,18 @@ export class PocCdkTypescriptStack extends cdk.Stack {
     // Create outputs for connecting
 
     // Output the private IP address of the EC2 instance
-    new cdk.CfnOutput(this, "IP Address", {
+    new CfnOutput(this, "IP Address", {
       value: ec2Instance.instancePrivateIp,
     });
 
     // Command to download the SSH key
-    new cdk.CfnOutput(this, "Download Key Command", {
+    new CfnOutput(this, "Download Key Command", {
       value:
         "aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > cdk-key.pem && chmod 400 cdk-key.pem",
     });
 
     // Command to access the EC2 instance using SSH
-    new cdk.CfnOutput(this, "ssh command", {
+    new CfnOutput(this, "ssh command", {
       value:
         "ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@" +
         ec2Instance.instancePrivateIp,
