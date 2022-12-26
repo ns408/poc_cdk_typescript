1a11d7697f8026a06c6c3b7a3b227e47c954992b
ns408 <nsrea408@gmail.com>
2022-12-26 16:20:12 +1100

    Fix the indentation.

--- lib/poc_cdk_typescript-stack.ts
+++ lib/poc_cdk_typescript-stack.ts
@@ -15,71 +15,71 @@ export class PocCdkTypescriptStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-      console.log(variablesFile.vpcid)
-      const vpc = ec2.Vpc.fromLookup(this, 'MyVPC',
-        {tags: {'aws-cdk:Name': variablesFile.vpcname}}
-      );
+    console.log(variablesFile.vpcid)
+    const vpc = ec2.Vpc.fromLookup(this, 'MyVPC',
+      {tags: {'aws-cdk:Name': variablesFile.vpcname}}
+    );
 
-      //ec2.Vpc.fromVpcAttributes(this, 'MyVPC', {
-      //  vpcId: variablesFile.vpcid,
-      //});
+    //ec2.Vpc.fromVpcAttributes(this, 'MyVPC', {
+    //  vpcId: variablesFile.vpcid,
+    //});
 
-      // Create a key pair to be used with this EC2 Instance
-      const key = new keypair.KeyPair(this, "KeyPair", {
-        name: "cdk-keypair",
-        description: "Key Pair created with CDK Deployment",
-      });
-      key.grantReadOnPublicKey;
+    // Create a key pair to be used with this EC2 Instance
+    const key = new keypair.KeyPair(this, "KeyPair", {
+      name: "cdk-keypair",
+      description: "Key Pair created with CDK Deployment",
+    });
+    key.grantReadOnPublicKey;
 
-      // Security group for the EC2 instance
-      const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
-        vpc,
-        description: "Allow SSH (TCP port 22) and HTTP (TCP port 80) in",
-        allowAllOutbound: true,
-      });
+    // Security group for the EC2 instance
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc,
+      description: "Allow SSH (TCP port 22) and HTTP (TCP port 80) in",
+      allowAllOutbound: true,
+    });
 
-      // Allow SSH access on port tcp/22
-      securityGroup.addIngressRule(
-        ec2.Peer.ipv4(variablesFile.whitelistIP),
-        ec2.Port.tcp(22),
-        "Allow SSH Access"
-      );
+    // Allow SSH access on port tcp/22
+    securityGroup.addIngressRule(
+      ec2.Peer.ipv4(variablesFile.whitelistIP),
+      ec2.Port.tcp(22),
+      "Allow SSH Access"
+    );
 
-      // Allow HTTP access on port tcp/80
-      securityGroup.addIngressRule(
-        ec2.Peer.ipv4(variablesFile.whitelistIP),
-        ec2.Port.tcp(80),
-        "Allow HTTP Access"
-      );
+    // Allow HTTP access on port tcp/80
+    securityGroup.addIngressRule(
+      ec2.Peer.ipv4(variablesFile.whitelistIP),
+      ec2.Port.tcp(80),
+      "Allow HTTP Access"
+    );
 
-      // IAM role to allow access to other AWS services
-      const role = new iam.Role(this, "ec2Role", {
-        assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
-      });
+    // IAM role to allow access to other AWS services
+    const role = new iam.Role(this, "ec2Role", {
+      assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+    });
 
-      // IAM policy attachment to allow access to
-      role.addManagedPolicy(
-        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
-      );
+    // IAM policy attachment to allow access to
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
 
-      // Look up the AMI Id for the Amazon Linux 2 Image with CPU Type X86_64
-      const ami = new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-        cpuType: ec2.AmazonLinuxCpuType.X86_64,
-      });
+    // Look up the AMI Id for the Amazon Linux 2 Image with CPU Type X86_64
+    const ami = new ec2.AmazonLinuxImage({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.X86_64,
+    });
 
-      // Create the EC2 instance using the Security Group, AMI, and KeyPair defined.
-      const ec2Instance = new ec2.Instance(this, "MyInstance", {
-        vpc,
-        instanceType: ec2.InstanceType.of(
-          ec2.InstanceClass.T2,
-          ec2.InstanceSize.MICRO
-        ),
-        machineImage: ami,
-        securityGroup: securityGroup,
-        keyName: key.keyPairName,
-        role: role,
-      });
+    // Create the EC2 instance using the Security Group, AMI, and KeyPair defined.
+    const ec2Instance = new ec2.Instance(this, "MyInstance", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ami,
+      securityGroup: securityGroup,
+      keyName: key.keyPairName,
+      role: role,
+    });
 
   }
 }
